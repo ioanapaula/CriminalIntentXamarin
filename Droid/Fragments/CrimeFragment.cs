de549459d3fcf655c31fe74fa5f43a745600b99f
Ioana-Paula Stroe <stroe.ioanapaula@gmail.com>
2019-03-21 17:15:46 +0200

    Challenge: More Dialogs

--- Droid/Fragments/CrimeFragment.cs
+++ Droid/Fragments/CrimeFragment.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using Android.App;
 using Android.Content;
+using Android.Icu.Text;
 using Android.OS;
 using Android.Support.V4.App;
+using Android.Support.V7.App;
 using Android.Text;
 using Android.Views;
 using Android.Widget;
@@ -10,16 +13,19 @@ using Java.Util;
 
 namespace CriminalIntentXamarin.Droid.Data
 {
-    public class CrimeFragment : Fragment
+    public class CrimeFragment : Android.Support.V4.App.Fragment
     {
         private const string ArgCrimeId = "crime_id";
         private const string DialogDate = "date_picker";
+        private const string DialogTime = "time_picker";
         private const int RequestDate = 0;
+        private const int RequestTime = 1;
 
         private Crime _crime;
         private CrimeLab _crimeLab;
         private EditText _titleField;
         private Button _dateButton;
+        private Button _timeButton;
         private CheckBox _solvedCheckBox;
 
         public static CrimeFragment NewInstance(UUID crimeId)
@@ -48,10 +54,15 @@ namespace CriminalIntentXamarin.Droid.Data
             var view = inflater.Inflate(Resource.Layout.fragment_crime, container, false);
             InitFields(view);
 
+            SetDateButtonText();
+            _dateButton.Click += DateButtonClicked;
+
+            SetTimeButtonText();
+            _timeButton.Click += TimeButtonClicked;
+
             _titleField.Text = _crime.Title;
             _titleField.TextChanged += TextChanged;
-            _dateButton.Text = _crime.Date.ToString();
-            _dateButton.Click += DateButtonClicked;
+
             _solvedCheckBox.Checked = _crime.Solved;
             _solvedCheckBox.CheckedChange += CheckBoxChecked;
 
@@ -60,12 +71,31 @@ namespace CriminalIntentXamarin.Droid.Data
 
         public override void OnActivityResult(int requestCode, int resultCode, Intent data)
         {
+            if (resultCode != (int)Result.Ok)
+            {
+                return;
+            }
+
+            var date = (Date)data.GetSerializableExtra(DatePickerFragment.ExtraDate);
+            _crime.Date = date;
+
             if (requestCode == RequestDate)
             {
-                var date = (Date)data.GetSerializableExtra(DatePickerFragment.ExtraDate);
-                _crime.Date = date;
-                _dateButton.Text = _crime.Date.ToString();
+                SetDateButtonText();
             }
+
+            if (requestCode == RequestTime)
+            {
+                SetTimeButtonText();
+            }
+        }
+
+        private void TimeButtonClicked(object sender, EventArgs e)
+        {
+            var fm = Activity.SupportFragmentManager;
+            var dialog = TimePickerFragment.NewInstance(_crime.Date);
+            dialog.SetTargetFragment(this, RequestTime);
+            dialog.Show(fm, DialogTime);
         }
 
         private void DateButtonClicked(object sender, EventArgs e)
@@ -90,7 +120,20 @@ namespace CriminalIntentXamarin.Droid.Data
         {
             _titleField = view.FindViewById<EditText>(Resource.Id.crime_title);
             _dateButton = view.FindViewById<Button>(Resource.Id.crime_date);
+            _timeButton = view.FindViewById<Button>(Resource.Id.crime_time);
             _solvedCheckBox = view.FindViewById<CheckBox>(Resource.Id.crime_solved);
+        }
+
+        private void SetDateButtonText()
+        {
+            var simpleFormat = new SimpleDateFormat("EEEE, MMM d, yyyy");
+            _dateButton.Text = simpleFormat.Format(_crime.Date);
+        }
+
+        private void SetTimeButtonText()
+        {
+            var timeFormat = new SimpleDateFormat("h:mm a");
+            _timeButton.Text = timeFormat.Format(_crime.Date);
         }
     }
 }
---
+++ Droid/Fragments/TimePickerFragment.cs
@@ -0,0 +1,80 @@
+﻿
+using System;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Support.V4.App;
+using Android.Views;
+using Android.Widget;
+using Java.Util;
+
+namespace CriminalIntentXamarin.Droid.Fragments
+{
+    public class TimePickerFragment : Android.Support.V4.App.DialogFragment
+    {
+        public const string ExtraDate = "com.companyname.criminalintentxamarin.crime_date";
+        private const string ArgDate = "date";
+
+        private Date _date;
+        private TimePicker _timePicker;
+
+        public static TimePickerFragment NewInstance(Date date)
+        {
+            var args = new Bundle();
+            args.PutSerializable(ArgDate, date);
+
+            var fragment = new TimePickerFragment
+            {
+                Arguments = args
+            };
+
+            return fragment;
+        }
+
+        public override Dialog OnCreateDialog(Bundle savedInstanceState)
+        {
+            _date = (Date)Arguments.GetSerializable(ArgDate);
+            var calendar = Calendar.Instance;
+            calendar.Time = _date;
+            var hour = calendar.Get(CalendarField.Hour);
+            var minutes = calendar.Get(CalendarField.Minute);
+
+            var view = LayoutInflater.From(Activity).Inflate(Resource.Layout.dialog_time, null);
+
+            _timePicker = view.FindViewById<TimePicker>(Resource.Id.dialog_time_picker);
+            _timePicker.Hour = hour;
+            _timePicker.Minute = minutes;
+
+            return new Android.Support.V7.App.AlertDialog.Builder(Activity)
+                .SetView(view)
+                .SetTitle(Resource.String.time_picker_title)
+                .SetPositiveButton(Android.Resource.String.Ok, OkAction)
+                .Create();
+        }
+
+        private void OkAction(object sender, DialogClickEventArgs e)
+        {
+            var hour = _timePicker.Hour;
+            var minute = _timePicker.Minute;
+
+            var calendar = Calendar.Instance;
+            calendar.Time = _date;
+            calendar.Set(CalendarField.Hour, hour);
+            calendar.Set(CalendarField.Minute, minute);
+
+            SendResult((int)Result.Ok, calendar.Time);
+        }
+
+        private void SendResult(int resultCode, Date date)
+        {
+            if (TargetFragment == null)
+            {
+                return;
+            }
+
+            var intent = new Intent();
+            intent.PutExtra(ExtraDate, date);
+            TargetFragment.OnActivityResult(TargetRequestCode, resultCode, intent);
+        }
+    }
+}
