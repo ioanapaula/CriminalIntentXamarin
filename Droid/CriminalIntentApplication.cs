5810570c49fc51a5e6150b6dc8cb2081107fe20e
Ioana-Paula Stroe <stroe.ioanapaula@gmail.com>
2019-03-26 15:15:05 +0200

    Fixed crash caused by missing constructor in CriminalIntentApplication

--- Droid/CriminalIntentApplication.cs
+++ Droid/CriminalIntentApplication.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using Android.App;
+using Android.Runtime;
 
 namespace CriminalIntentXamarin.Droid
 {
     [Application(AllowBackup = false)]
     public class CriminalIntentApplication : Application
     {
-        public CriminalIntentApplication()
+        public CriminalIntentApplication(IntPtr handle, JniHandleOwnership ownerShip) : base(handle, ownerShip)
         {
         }
     }
